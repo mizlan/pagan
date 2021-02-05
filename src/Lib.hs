415ea1591e1c45aad72c685bc290630137368645
Michael Lan <michaellan202@gmail.com>
2021-02-04 18:49:41 -0800

    add config support

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,6 +11,8 @@ import           System.Directory               ( listDirectory
 
 import           System.FilePath                ( (</>) )
 
+type Config = (String, Maybe BuildCommand, RunCommand)
+
 -- |Takes in a directory and gets the most recently supported file
 -- getMostRecentSupportedFile :: FilePath -> Maybe FilePath
 -- getMostRecentSupportedFile path = do
@@ -31,3 +33,33 @@ getMostRecentEntry :: [FilePath] -> IO FilePath
 getMostRecentEntry contents = do
   records <- mapM getModificationTime contents
   return $ fst $ maximumBy (comparing snd) $ zip contents records
+
+getConfigLanguage :: Config -> String
+getConfigLanguage (lang, _, _) = lang
+
+getConfigBuildCommand :: Config -> Maybe BuildCommand
+getConfigBuildCommand (_, cmd, _) = cmd
+
+getConfigRunCommand :: Config -> BuildCommand
+getConfigRunCommand (_, _, cmd) = cmd
+
+
+-- |Returns if a file is supported by a list of configurations
+isSupportedBy :: FilePath -> [Config] -> Bool
+isSupportedBy fileconfigs = any (`isExtensionOf` file) (getConfigLanguage <$> configs)
+
+getDefaultConfig :: [Config]
+getDefaultConfig =
+  -- lang    build command (if any)                 run command
+  ----------------------------------------------------------------
+  [ ("hs"  , Just "ghc FILE"                      , "./BASE")
+  , ("cpp" , Just "g++-10 -std=c++11 FILE"        , "./a.out")
+  , ("py"  , Nothing                              , "python FILE")
+  , ("java", Just "javac FILE"                    , "java BASE")
+  , ("c"   , Just "gcc FILE -o BASE"              , "./BASE")
+  , ("ml"  , Just "ocamlopt str.cmxa -unsafe FILE", "./a.out")
+  , ("jl"  , Nothing                              , "julia FILE")
+  ]
+
+getSupportedFiles :: [FilePath] -> [Config] -> [FilePath]
+getSupportedFiles contents = filter isSupportedBy
